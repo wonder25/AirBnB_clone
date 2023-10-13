5dc15fb6f5b67fc9d271d75d9b7200b81d13a577
wonder25 <wonderfulmwinzi14@gmail.com>
2023-10-14 02:04:34 +0300

    added User Module

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+User Module
+"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    User class
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
