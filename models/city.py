7b6bbb4511515e98064a17f26c96996271f7fe40
wonder25 <wonderfulmwinzi14@gmail.com>
2023-10-14 02:15:33 +0300

    added city module

---
+++ models/city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+City Module
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    City class
+    """
+
+    state_id = ""
+    name = ""
